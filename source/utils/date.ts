13446c465effde55af064346d7c1d7fa85c4b41a
Luis Jacobetty <luisjs92@gmail.com>
2021-06-28 18:40:52 +0100

    Modernize date solution with new date formatter fn

--- source/utils/date.ts
+++ source/utils/date.ts
@@ -1,4 +1,15 @@
-import { padStart } from "lodash";
+/**
+ * Attempts to format a given date.
+ * @param date Date to format.
+ * @param locale Locale/region to use for formatting options.
+ * @param parts Parts for the final formatted date.
+ * @param separator Separator to append between the given parts.
+ */
+export function formatDateIntl(date: Date, locale: string, parts: Intl.DateTimeFormatOptions[], separator: string): string {
+    return parts
+        .map((part) => new Intl.DateTimeFormat(locale, part).format(date))
+        .join(separator);
+};
 
 /**
  * Formats a date to "YYYY-MM-DD".
@@ -14,9 +25,9 @@ export function formatYMD(baseDate: Date, offsetInDays: number = 0): string {
         dateObj = new Date(timeOffset);
     }
 
-    return [
-        dateObj.getFullYear(),
-        padStart(`${dateObj.getMonth() + 1}`, 2, "0"),
-        padStart(`${dateObj.getDate()}`, 2, "0")
-    ].join('-');
+    return formatDateIntl(dateObj, "default", [
+        { year: "numeric" },
+        { month: "2-digit" },
+        { day: "2-digit" }
+    ], "-");
 };
